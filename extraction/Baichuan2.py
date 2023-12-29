072ceda8a9f7b6aaf23e9392ff68225b4fa1466f
Harry_FBK <40137928+hellocym@users.noreply.github.com>
2023-12-29 17:09:58 +0800

    use base

--- extraction/Baichuan2.py
+++ extraction/Baichuan2.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("baichuan-inc/Baichuan2-7B-Base", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("baichuan-inc/Baichuan2-7B-Chat", torch_dtype=torch.float16, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("baichuan-inc/Baichuan2-7B-Base", torch_dtype=torch.float16, trust_remote_code=True)
 model = model.quantize(8).cuda() 
 inputs = tokenizer('登鹳雀楼->王之涣\n夜雨寄北->', return_tensors='pt')
 inputs = inputs.to('cuda:0')
