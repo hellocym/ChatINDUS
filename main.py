604e015672dcbb66da0f9aa22ba82f9b515085f0
Harry_FBK <40137928+hellocym@users.noreply.github.com>
2024-01-23 17:44:37 +0800

    exp

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     ext = Extraction()
     while True:
         inp = input("Input: ")
-        inp = inp.strip(',')
-        inp = inp.strip('"')
-        inp = eval(inp)
-        inp = [i['user'] for i in inp]
-        inp = ' '.join(inp)
+        # inp = inp.strip(',')
+        # inp = inp.strip('"')
+        # inp = eval(inp)
+        # inp = [i['user'] for i in inp]
+        # inp = ' '.join(inp)
         print(inp)
         ans = ext.extract(inp)
         print(ans)
