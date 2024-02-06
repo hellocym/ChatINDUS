68621e28c6d9c002b83eca8b6e55c8d314da2144
Harry_FBK <40137928+hellocym@users.noreply.github.com>
2024-02-06 23:55:36 +0800

    return to postgresql

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# from extraction.Baichuan2 import Extraction
-from extraction.XVERSE import Extraction
-# from 
+import os
 
-# # use hf mirror site
-# import os
+from extraction.XVERSE import Extraction
+
+# use hf mirror site
 # os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+
 
 if __name__ == "__main__":
     ext = Extraction()
+    os.system("service postgresql start")
+    print("Postgres started")
     while True:
         inp = input("Input: ")
         # inp = inp.strip(',')
@@ -17,5 +19,5 @@ if __name__ == "__main__":
         # inp = ' '.join(inp)
         print(inp)
         ans = ext.extract(inp)
-
         print(ans)
+        
--- query/postgre.py
+++ query/postgre.py
@@ -3,33 +3,38 @@ from psycopg2 import sql
 import pandas as pd
 import csv
 
-conn = psycopg2.connect(
-    dbname="postgres",
-    user="postgres",
-    password="postgres",
-    host="localhost",
-    port="5432"
-)
+if __name__ == '__main__':
+    import os
+    os.system("service postgresql start")
+    print("Postgres started")
 
-cur = conn.cursor()
-
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS commodities (
-        commodity_code BIGINT PRIMARY KEY,
-        commodity_name VARCHAR(255),
-        type_gauge VARCHAR(255),
-        class_name VARCHAR(255),
-        class_code VARCHAR(255),
-        commodity_specific JSON
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432"
     )
-""")
 
-with open('data/commodity.csv', 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    next(reader)  # 跳过标题行
-    for row in reader:
-        cur.execute("""
-            INSERT INTO commodities (commodity_code, commodity_name, type_gauge, class_name, class_code, commodity_specific)
-            VALUES (%s, %s, %s, %s, %s, %s)
-            ON CONFLICT (commodity_code) DO NOTHING
-        """, row)
+    cur = conn.cursor()
+
+    # cur.execute("""
+    #     CREATE TABLE IF NOT EXISTS commodities (
+    #         commodity_code BIGINT PRIMARY KEY,
+    #         commodity_name VARCHAR(255),
+    #         type_gauge VARCHAR(255),
+    #         class_name VARCHAR(255),
+    #         class_code VARCHAR(255),
+    #         commodity_specific JSON
+    #     )
+    # """)
+
+    # with open('data/commodity.csv', 'r', encoding='utf-8') as file:
+    #     reader = csv.reader(file)
+    #     next(reader)  # 跳过标题行
+    #     for row in reader:
+    #         cur.execute("""
+    #             INSERT INTO commodities (commodity_code, commodity_name, type_gauge, class_name, class_code, commodity_specific)
+    #             VALUES (%s, %s, %s, %s, %s, %s)
+    #             ON CONFLICT (commodity_code) DO NOTHING
+    #         """, row)
