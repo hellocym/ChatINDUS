2b90d5827a79912069b78c992cf63775a7234976
Harry_FBK <40137928+hellocym@users.noreply.github.com>
2024-02-28 16:49:51 +0800

    fix import

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-from ChatINDUS.extraction.XVERSE import Extraction
-from ChatINDUS.query.postgre import Postgre
+from .extraction.XVERSE import Extraction
+from .query.postgre import Postgre
 import torch
 
 # use hf mirror site
